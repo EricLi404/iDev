fed3ed7e7baac099b2367b072aade7c435ee13e1
EricLi404 <leiflyy@outlook.com>
2021-04-15 15:41:30 +0800

    feat(breathe): add main function

--- breathe/Shared/ContentView.swift
+++ breathe/Shared/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var breathIn = true
+    @State private var breathOut = false
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            VStack{
+                Text("沉静下来,")
+                    .font(.largeTitle)
+                    .bold()
+                Text("将注意力放在呼吸上")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+            }
             .padding()
+            VStack{
+                FlowerView()
+                HStack{
+                    Text("呼气")
+                        .font(.title)
+                        .opacity(breathOut ? 0.1 : 1)
+                        .animation(Animation.easeInOut(duration: 4.5).delay(0.5).repeatForever())
+                        .onAppear{
+                            breathOut.toggle()
+                        }
+                        .padding()
+                    Text("吸气")
+                        .font(.title)
+                        .opacity(breathIn ? 0.1 : 1)
+                        .animation(Animation.easeInOut(duration: 4.5).delay(0.5).repeatForever())
+                        .onAppear{
+                            breathIn.toggle()
+                        }
+                }
+               
+            }.padding()
+
+        }
+       
     }
 }
 
@@ -19,3 +54,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
