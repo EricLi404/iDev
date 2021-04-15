f2d85c948aa0200993d9df113a4c06ecd32694bb
EricLi404 <leiflyy@outlook.com>
2021-04-15 15:33:07 +0800

    feat(breathe): add swiftUI hello world

---
+++ breathe/Shared/breatheApp.swift
@@ -0,0 +1,17 @@
+//
+//  breatheApp.swift
+//  Shared
+//
+//  Created by Eric Li on 2021/4/15.
+//
+
+import SwiftUI
+
+@main
+struct breatheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
